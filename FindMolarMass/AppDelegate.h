754333c73e0393071df92576f8c14c13e62cd26a
mobileappscloud <doc92606@gmail.com>
2014-04-27 15:02:52 -0700

    Initial Commit

---
+++ FindMolarMass/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  FindMolarMass
+//
+//  Created by iMac on 4/27/14.
+//  Copyright (c) 2014 CongenialApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
